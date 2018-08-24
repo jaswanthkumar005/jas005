683136dcceb063392ce04953af660a4b6f3ed668
jaswanthkumar005 <31206728+jaswanthkumar005@users.noreply.github.com>
2018-08-24 16:34:17 +0530

    error in hashset

---
+++ DuplicateArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+public class  DuplicateArray{
+    public void duplicateVal(String[] str){
+        
+        ArrayList<String> obj = new ArrayList<String>(Arrays.asList(str));
+         
+        System.out.println(obj);
+        HashSet<String> hash = new HashSet<String>();
+          for(String sh : obj){
+              if(!hash.contains(sh)){
+                 hash.add(sh); 
+              }else{
+                  
+              }
+          }
+          System.out.println(hash);
+        }
+    
+  public static void main(String a[]){
+    DuplicateArray obj = new DuplicateArray();
+    System.out.println("String: BeginnersBook.com");
+    System.out.println("-------------------------");
+    obj.duplicateVal(new String[] {"Abbai","red","black","red","Abbai"});
+  
+    
+  }
+}
