87b5032a3ff969db66e37b2307ead3daba967dca
jaswanthkumar005 <31206728+jaswanthkumar005@users.noreply.github.com>
2019-04-04 12:02:54 +0530

    increasing weight

--- increasingWeight.js
+++ increasingWeight.js
@@ -1,31 +1,60 @@
+ 
 var arr=[10,36,54,89,12];
 var obj={};
-
+ var sortedArr=[];
 for(var i=0;i<arr.length;i++){
 
     var n= arr[i];
-
-    if(n % Math.sqrt(n)== 0 && n % 6 == 0 && n%4 == 0 &&  n % 2 == 0){
-
-       obj[n]= 12;
-
-    }else if(n % Math.sqrt(n)==0 && n % 6 == 0 && n%4 == 0 ){
-        obj[n]= 5+4;
-    }else if(n % Math.sqrt(n)==0 && n % 2 == 0 ){
-        obj[n]= 5+3;
-    }else if( n % 6 == 0 && n%4 == 0 && n % 2 == 0){
-        obj[n]= 4+3;
-    }else if( n % 24 == 0){
-        obj[n]= 4;
-    }else if(n % 2 == 0){
-        obj[n]= 3;
-    }else if(n % Math.sqrt(n) == 0){
-        obj[n]= 5;
-    }else{
-        obj[n]=0;
+	obj[n]=0;
+	if(n % Math.sqrt(n) == 0){
+        obj[n]+= 5;
     }
+	if(n % 4 == 0 && n % 6 == 0 ){
+        obj[n]+= 4;
+    }
+	if(n % 2 == 0){
+        obj[n]+= 3;
+    }
+	
+ 
+   
 }
 
 
-var keysSorted = Object.keys(obj).sort(function(a,b){return obj[a]-obj[b]});
-console.log(keysSorted);
+ objSort(obj);
+
+ function objSort(obj){
+ 
+ var min=0;
+ var minObj=0;
+  if(Object.keys(obj).length == undefined || Object.keys(obj).length == 0){
+
+ return 0;
+ }
+ 
+ for(var x in obj){
+
+    if(obj[x]>=min){
+	   min=obj[x];
+	   minObj=x;
+	   
+      
+	}
+ 
+ }
+   
+	 // console.log(x+"z");
+	 // console.log(obj[min]);
+	  //console.log(obj[minObj] );
+	  delete obj[minObj];
+	  sortedArr.unshift(parseInt(minObj));
+
+
+
+ 
+
+  return objSort(obj);
+  
+ }
+ 
+   console.log(sortedArr);
